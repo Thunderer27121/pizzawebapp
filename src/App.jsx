6ad9e2cd1b6fbc1f4aee2ccb72b8f63c31ba8c67
Thunderer27121 <anujsingh27121@gmail.com>
2025-08-08 17:57:42 +0530

    one

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import Navbar from "./components/Navbar"
 import Approutes from "./components/Approutes"
+import { useEffect } from "react"
+import { useLocation } from "react-router-dom"
 const App = () => {
+  const {pathname} = useLocation();
+  useEffect(() => {
+    window.scrollTo(0,0)
+  }, [pathname])
+  
   return (
     <div className=" h-screen w-full bg-black text-white overflow-auto" style={{scrollbarWidth: "none"}}>
       <Navbar/>
