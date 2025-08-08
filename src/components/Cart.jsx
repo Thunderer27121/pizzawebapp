8012ee9d21063f42eb691fbb114333b1aed0bc74
Thunderer27121 <anujsingh27121@gmail.com>
2025-08-08 18:05:57 +0530

    three

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -9,13 +9,13 @@ const Cart = () => {
     const cartItems = useSelector((state) => state.cart.cartItems);
 
     return (
-        <div className="max-w-4xl mx-auto p-6 flex-col md:flex justify-center items-center">
+        <div className="max-w-4xl mx-auto p-6 flex-col flex justify-center items-center">
             <h2 className="text-3xl font-bold mb-6 text-center">🛒 Your Cart</h2>
 
             {cartItems.length === 0 ? (
-                <p className="text-center text-gray-500 flex-col flex w-1/2 ml-55 ">Your cart is empty
+                <p className="text-center text-gray-500 flex flex-col justify-center w-1/2  ">Your cart is empty
                     <button className="border-amber-50 outline-2 rounded-xl p-2 mt-10" onClick={() => { navigate(-1) }}>GO BACK</button>
-                    <img src="https://www.pngplay.com/wp-content/uploads/7/Cart-Transparent-PNG.png" alt="" className='rounded-2xl mt-12 hover:bg-white transition' />
+                    <img src="https://www.pngplay.com/wp-content/uploads/7/Cart-Transparent-PNG.png" alt="" className='rounded-2xl mt-12 hover:bg-white transition h-[200px] w-[200px] m-auto' />
                 </p>
             ) : (
                 <div className="space-y-6">
