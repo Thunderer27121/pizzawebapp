aea2970fb71011a7229ea1636045525e9b24f2f6
Thunderer27121 <anujsingh27121@gmail.com>
2025-08-08 18:00:44 +0530

    two

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -9,7 +9,7 @@ const Cart = () => {
     const cartItems = useSelector((state) => state.cart.cartItems);
 
     return (
-        <div className="max-w-4xl mx-auto p-6 flex-col justify-center items-center">
+        <div className="max-w-4xl mx-auto p-6 flex-col md:flex justify-center items-center">
             <h2 className="text-3xl font-bold mb-6 text-center">🛒 Your Cart</h2>
 
             {cartItems.length === 0 ? (
